d996e2e73e454931fb38c3c8bdb75d790d51dcd6
zion livingstone <zionlivingstone@gmail.com>
2022-09-16 19:26:17 +0100

    99

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@https://github.com/Stoneybro/Quizapp main:gh-pages
+git push -f git@github.com/Stoneybro/Quizapp.git main:gh-pages
 
 cd -
